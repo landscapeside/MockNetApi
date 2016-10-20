3b8ba56dc4d881e100a2c39874b99584547e800d
jiangshun <494326656@qq.com>
2016-10-20 16:56:38 +0800

    1

---
+++ app/src/main/java/com/landscape/mocknetapi/MainActivity.java
@@ -0,0 +1,22 @@
+package com.landscape.mocknetapi;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.webkit.CookieManager;
+import android.webkit.CookieSyncManager;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ mockapi/src/main/java/com/landscape/mockapi/ApiSuite.java
@@ -0,0 +1,8 @@
+package com.landscape.mockapi;
+
+/**
+ * Created by landscape on 2016/10/19.
+ */
+
+public class ApiSuite {
+}
